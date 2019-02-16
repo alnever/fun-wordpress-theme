ac292f665d28a0527e58d9337109ec2ba933feed
Aleksei Neverov <al_neverov@live.ru>
2019-02-16 15:41:43 +0500

    Widgets and froms styling in process

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="<?php _e(get_template_directory_uri()); ?>/js/index.js"></script>
 <?php wp_footer(); ?>
 </body>
 </html>
--- js/index.js
+++ js/index.js
@@ -1,6 +1,53 @@
 (function () {
-	'use strict';
+    'use strict';
+
+    /**
+     * Archive Widget: wrap number of records with span
+     */
+
+    $(function() {
+        $('.widget_archive > ul > li').each(function() {
+            $(this).html(
+                $(this).html().replace(/\((\d+)\)/ig, '<span class="widget-archive-count">$1</span>')
+            );
+        });
+    });
+
+    /**
+     * Categories Widget: wrap number of records with span
+     */
+
+    $(function() {
+        $('.widget_categories > ul > li').each(function() {
+            $(this).html(
+                $(this).html().replace(/\((\d+)\)/ig, '<span class="widget-category-count">$1</span>')
+            );
+        });
+    });
+
+    /**
+     * Categories Widget: mark categories with children
+     */
+
+    $(function() {
+        $('li.cat-item').has('ul.children').addClass('super-category');
+
+        $('li.super-category').each(function() {
+            $(this).children("a, span")
+                .wrapAll('<div class="super-category-title"></div>');
+        });
+    });
 
 
+    /**
+     * Tags cloud - remove brakets
+     */
+    $(function() {
+        $('.tag-link-count').each(function() {
+            $(this).text(
+                $(this).text().replace(/\((\d+)\)/i, '$1')
+            );
+        });
+    });
 
 }());
